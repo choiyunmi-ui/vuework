bd54e970aa6563124cc985f747084be2f905afa2
choiyunmi-ui <chym8872@nate.com>
2022-11-20 18:06:11 +0900

    #221120

---
+++ LectureMaterial/Vuejs/vue30컴포넌트실습/comprj01/src/components/vue3403/Vue3403Comp.vue
@@ -0,0 +1,23 @@
+<style scoped></style>
+
+<template>
+  <div>
+    <button v-on:click="showLog">show</button>
+  </div>
+</template>
+
+<script>
+// vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
+// import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
+
+export default {
+  methods: {
+    /* 이벤트 핸들러 등록 + 일반 함수 */
+    showLog(e) {
+      console.log(e.target);
+      debugger;
+      this.$emit('show-log', 'abc', 123);
+    },
+  },
+};
+</script>
